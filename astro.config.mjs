6b6a55fdfa387dd82029cbd0e0b9e78ad2f1f25b
riskiipriyadi <riskipriyadi@protonmail.com>
2026-06-17 16:04:48 +0700

    fix: update base path for riskipriyadii deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
   site: 'https://riskipriyadi.github.io',
-  base: '/sinibaca-v2',
+  base: '/sinibaca/',
   output: 'static',
 });
